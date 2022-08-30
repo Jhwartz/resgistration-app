1702852ff0fcc52a9094a47515418d0a07249b6d
Jhwartz <jhwartz@yahoo.co.uk>
2022-08-30 14:02:48 +0000

    Adding up to the work on context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,15 +4,17 @@ import {createContext,useState} from 'react'
 const authContext = createContext();
 AuthProvider= authContext.Provider;
 
-function AuthContext() {
+function AuthContextProvider() {
     const [success, setSuccess] =useState('false');
     const [error, setError] =useState('null');
     const [loading, setLoading] =useState('false');
     const [user, setUser]=useState('null');
 
   return (
-    <div>AuthContext</div>
+    <div>
+      
+    </div>
   )
 }
 
-export default AuthContext
+export default AuthContextProvider
