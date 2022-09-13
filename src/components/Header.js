31749c47f0da1450d4eabbef6c1f2fc0f35a24a8
Jhwartz <80740009+Jhwartz@users.noreply.github.com>
2022-09-13 15:13:07 +0000

    login authorization

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,49 @@
 import React from 'react'
 import {useEffect, useState, useContext} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import { FaSignOutAlt, FaUser } from "react-icons/fa";
 
 function Header() {
     const navigate =useNavigate();
-    const {success, loading, user} = useContext()
+    const {success, loading, user, authLogout} = useContext(AuthContext)
+
+  useEffect (()=> {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user]);
   return (
-    <div>Header</div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Registration App</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+      </ul>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <FaUser />
+              {user.username}
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class ="nav-link" type="button"
+              onClick={() => authLogout()}
+              href="/"><FaSignOutAlt/></a>
+          </li>
+
+        </ul>
+    </div>
+  </div>
+</nav>
   )
 }
 
